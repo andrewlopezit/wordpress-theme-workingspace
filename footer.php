5a78b8b6dd6a345fc947f32725deec5a9db58dc8
Andrew Lopez <andrewlopezit>
2021-07-04 00:05:35 +0800

    created footer

--- footer.php
+++ footer.php
@@ -1,4 +1,11 @@
-<?php wp_footer(); ?>
+<div class="footer background">
+    <div class="container">
+        <div class="row">
+            <div class="col"></div>
+        </div>
+    </div>
+</div>
 
+<?php wp_footer(); ?>
 </body>
 </html>
