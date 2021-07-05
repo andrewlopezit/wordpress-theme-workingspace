7aad7d74de068bbf5696d0a1eb88c7fa212732f2
Andrew Lopez <andrewlopezit>
2021-07-05 22:54:11 +0800

    add customizer

---
+++ inc/customizer/ThemeCustomizer.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Workingspace functions and definitions
+ *
+ * @link https://developer.wordpress.org/themes/basics/theme-functions/
+ *
+ * @package WordPress
+ * @since Workingspace 1.0
+ */
+
+ namespace Inc\Customizer;
+
+ final class ThemeCustomier {
+
+    public function __consruct() {
+        
+    }
+ }
