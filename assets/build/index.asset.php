39be976f645a1faece0f384b70054766ae49a54a
Andrew Lopez <andrewlopezit>
2021-09-17 19:56:20 +0800

    adjusted small device position

--- assets/build/index.asset.php
+++ assets/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-polyfill'), 'version' => '19932adab09a764c31f5764c68ca77ca');
+<?php return array('dependencies' => array('wp-polyfill'), 'version' => '1967215a43a7346d51d7dbdbfc09f8f2');
